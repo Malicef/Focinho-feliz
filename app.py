1b2f66b8e0a3dc2d5e7293a40c8f081bc14e9b08
FernandaRFs <Fernandarochafernandes95@gmail.com>
2025-07-22 16:25:45 -0300

    resolvendo conflito

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, jsonify, session
 from flask_cors import CORS
-from db import db, Usuario, criar_tabelas_teste
+from db import db, Usuario, criar_tabelas_teste, Cachorro
+import os
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = os.path.join("static", "uploads")
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Inicializa o app Flask
 app = Flask(__name__)
 app.secret_key = "chave-secreta"  # necessário para usar session
 CORS(app, supports_credentials=True)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # Rota da API de login
 @app.route("/api/login", methods=["POST"])
@@ -21,9 +29,37 @@ def login_usuario():
     else:
         return jsonify({"erro": "Email ou senha inválidos"}), 401
 
+
+@app.route("/api/cadastrar-pet", methods=["POST"])
+def cadastrar_pet():
+    if "usuario_id" not in session:
+        return jsonify({"erro": "Usuário não autenticado"}), 401
+
+    nome = request.form.get("nome")
+    peso = request.form.get("peso")
+    foto = request.files.get("foto")
+
+    if not nome or not peso or not foto:
+        return jsonify({"erro": "Todos os campos são obrigatórios"}), 400
+
+    # Salvar imagem com nome seguro
+    filename = secure_filename(foto.filename)
+    caminho_foto = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    foto.save(caminho_foto)
+
+    # Grava no banco
+    Cachorro.create(
+        nome=nome,
+        peso=float(peso),
+        foto=caminho_foto  # caminho para exibição posterior
+    )
+
+    return jsonify({"mensagem": "Pet cadastrado com sucesso"})
+
+
 # Iniciar servidor
 if __name__ == '__main__':
     db.connect()
-    db.create_tables([Usuario])
+    db.create_tables([Usuario, Cachorro])
     criar_tabelas_teste()
     app.run(debug=True)
--- db.py
+++ db.py
@@ -14,3 +14,12 @@ class Usuario(Model):
 def criar_tabelas_teste():
     if not Usuario.get_or_none(Usuario.email == "usuario.com"):
         Usuario.create(email="usuario.com", senha="1234")
+        
+
+class Cachorro(Model):
+    nome = CharField()
+    peso = FloatField()
+    foto = CharField()
+    
+    class Meta:
+        database = db
